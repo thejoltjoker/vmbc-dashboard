b86ca4d448544214f8c9e040623c798c2d0e9fa3
Johannes Andersson <hello@thejoltjoker.com>
2023-11-30 17:48:09 +0000

    Add basic error handling to club

--- api/src/functions/club.ts
+++ api/src/functions/club.ts
@@ -4,27 +4,37 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 export async function club(
   request: HttpRequest,
-  context: InvocationContext
+  context: InvocationContext,
 ): Promise<HttpResponseInit> {
-  
-  let headersList = {
-    Accept: "*/*",
-    Authorization: `Bearer ${process.env.API_KEY}`,
-  };
+  try {
+    const headersList = {
+      Accept: "*/*",
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    };
 
-  let reqOptions = {
-    url: "https://api.brawlstars.com/v1/clubs/%23289VC0U29",
-    method: "GET",
-    headers: headersList,
-  };
+    const reqOptions = {
+      url: "https://api.brawlstars.com/v1/clubs/%23289VC0U29",
+      method: "GET",
+      headers: headersList,
+    };
 
-  let response = await axios.request(reqOptions);
+    const response: AxiosResponse = await axios.request(reqOptions);
 
-  return { jsonBody: response.data };
+    // Assuming you want to handle specific HTTP status codes as errors, you can customize this check.
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status code ${response.status}`);
+    }
+
+    return { jsonBody: response.data };
+  } catch (error) {
+    // Handle the error appropriately, you can log it, throw a custom error, or perform any other necessary actions.
+    console.error("Error during API request:", error.message);
+    return { body: error.message };
+  }
 }
 
 app.http("club", {
