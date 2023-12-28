0d858f3a2b843def5083a6378ea859b2bca1f931
Johannes Andersson <hello@thejoltjoker.com>
2023-12-28 19:57:53 +0000

    Create general stats box component

---
+++ src/components/player/stats/PlayerStatsBox.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import BoxGray from '@/components/boxes/BoxGray.vue'
+</script>
+
+<template>
+  <BoxGray>
+    <article class="p-3 md:p-5 flex flex-col gap-3 justify-between h-full">
+      <h4 class="uppercase font-medium text-zinc-500 text-sm">
+        <slot name="title">Default title</slot>
+      </h4>
+
+      <div class="inline-flex gap-2 items-center">
+        <slot name="content"></slot>
+      </div>
+
+      <div class="inline-flex gap-2 border-t-[1px] pt-2 border-zinc-100/10">
+        <div>
+          <slot name="svg"></slot>
+        </div>
+
+        <p class="text-zinc-500 text-sm">
+          <slot name="subtitle">Default subtitle</slot>
+        </p>
+      </div>
+    </article>
+  </BoxGray>
+</template>
--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { DBPlayer, PlayerBrawler } from '@/shared/models/player.model'
 import LoadingPage from '@/components/LoadingPage.vue'
 import PlayerHeader from '@/components/PlayerHeader.vue'
 import BoxGray from '@/components/boxes/BoxGray.vue'
@@ -8,11 +7,13 @@ import PlayerStatsStarPlayerStreak from '@/components/player/PlayerStatsStarPlay
 import PlayerStatsWinStreak from '@/components/player/PlayerStatsWinStreak.vue'
 import type { Brawler } from '@/models/brawler.model'
 import type { Icons } from '@/models/icon.model'
+import type { DBPlayer, PlayerBrawler } from '@/shared/models/player.model'
 import { useStorage, useTitle } from '@vueuse/core'
 import axios from 'axios'
 import _ from 'lodash'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import PlayerStatsTopBrawler from '@/components/player/stats/PlayerStatsTopBrawler.vue'
 const route = useRoute()
 
 const loading = ref(false)
@@ -277,45 +278,8 @@ watch(
         </BoxGray>
         <PlayerStatsWinStreak :player="player" />
         <PlayerStatsStarPlayerStreak :player="player" />
-        <BoxGray>
-          <div class="p-3 md:p-5 flex flex-col gap-3 justify-between h-full">
-            <p class="uppercase font-medium text-zinc-500 text-sm">Top brawler</p>
-            <div class="inline-flex gap-2 items-center">
-              <div class="w-20 h-20">
-                <img
-                  :src="brawlers.find((brawler) => brawler.id == topBrawler?.id)?.imageUrl3"
-                  alt=""
-                  class="-mt-1"
-                />
-              </div>
-              <p class="font-display uppercase font-bold text-white text-6xl">
-                {{ topBrawler?.name }}
-              </p>
-            </div>
+        <PlayerStatsTopBrawler :topBrawler="topBrawler" :brawlers="brawlers" />
 
-            <div class="inline-flex gap-2 border-t-[1px] pt-2 border-zinc-100/10">
-              <div class="text-amber-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  class="w-5 h-5"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 1c-1.828 0-3.623.149-5.371.435a.75.75 0 00-.629.74v.387c-.827.157-1.642.345-2.445.564a.75.75 0 00-.552.698 5 5 0 004.503 5.152 6 6 0 002.946 1.822A6.451 6.451 0 017.768 13H7.5A1.5 1.5 0 006 14.5V17h-.75C4.56 17 4 17.56 4 18.25c0 .414.336.75.75.75h10.5a.75.75 0 00.75-.75c0-.69-.56-1.25-1.25-1.25H14v-2.5a1.5 1.5 0 00-1.5-1.5h-.268a6.453 6.453 0 01-.684-2.202 6 6 0 002.946-1.822 5 5 0 004.503-5.152.75.75 0 00-.552-.698A31.804 31.804 0 0016 2.562v-.387a.75.75 0 00-.629-.74A33.227 33.227 0 0010 1zM2.525 4.422C3.012 4.3 3.504 4.19 4 4.09V5c0 .74.134 1.448.38 2.103a3.503 3.503 0 01-1.855-2.68zm14.95 0a3.503 3.503 0 01-1.854 2.68C15.866 6.449 16 5.74 16 5v-.91c.496.099.988.21 1.475.332z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-
-              <p class="text-zinc-500 text-sm">
-                <span class="text-white"> {{ topBrawler?.trophies }} </span>
-                Trophies
-              </p>
-            </div>
-          </div>
-        </BoxGray>
         <BoxGray>
           <div class="p-3 md:p-5 flex flex-col gap-3 justify-between h-full">
             <p class="uppercase font-medium text-zinc-500 text-sm">Favorite brawler</p>
